815fbaeab6afa2485ba09c5643fe18036f5ba2f2
apoorv jain <apoorv127@gmail.com>
2024-05-05 14:17:22 +0530

    05th May 2024

--- src/main/java/org/selenium/aj/utils/Helper.java
+++ src/main/java/org/selenium/aj/utils/Helper.java
@@ -5,20 +5,28 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.io.FileHandler;
-import org.selenium.aj.driver.DriverManagerTL;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Helper {
 
     public static void captureScreenshot(WebDriver driver){
         File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
         try {
-            FileHandler.copy(src,new File("src/Screenshots/Login.png"));
+            FileHandler.copy(src,new File("src/Screenshots/"+getCurrentDateTime()+".png"));
             System.out.println("Screenshot Captured");
         } catch (IOException e) {
             System.out.println("Unable to capture screenshot "+e.getMessage());
         }
+    }
+
+    public static String getCurrentDateTime(){
+        DateFormat customFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
+        Date currentDate = new Date();
+        return customFormat.format(currentDate);
     }
 }
